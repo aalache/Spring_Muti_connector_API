832e43238ce8ed047321bd4b596ffcf70bd7eb25
oaalache <oaalache@kali>
2025-01-26 14:58:34 +0100

    initial commit

---
+++ src/main/java/com/multi_connector/spring_multi_connector/SpringMultiConnectorApplication.java
@@ -0,0 +1,13 @@
+package com.multi_connector.spring_multi_connector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMultiConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMultiConnectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/multi_connector/spring_multi_connector/SpringMultiConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multi_connector.spring_multi_connector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMultiConnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
